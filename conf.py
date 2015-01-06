e470185cfd97f2f1e8c86d371c8eaa46e0a685f6
Christophe Coevoet <stof@notk.org>
2015-01-06 01:39:09 +0100

    Add the Travis configuration

--- conf.py
+++ conf.py
@@ -12,8 +12,8 @@
 # serve to show the default.
 
 import sys, os
-from sphinx.highlighting import lexers 
-from pygments.lexers.web import PhpLexer 
+from sphinx.highlighting import lexers
+from pygments.lexers.web import PhpLexer
 
 lexers['php'] = PhpLexer(startinline=True)
 
@@ -131,7 +131,7 @@ html_context = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['theme/_static']
+#html_static_path = ['theme/_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
